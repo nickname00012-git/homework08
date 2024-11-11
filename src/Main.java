3d774ce4e0a5f8148f0bb71154cd48b87831f801
nickname00012-git <info@malex.su>
2024-11-11 21:42:47 +0300

    Homework08 - done.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -17,13 +19,9 @@ public class Main {
         System.out.println("=======");
         System.out.println();
 
-        int deposit = 0;
-        int monthNumber = 0;
-        while (deposit <= 2_459_000) {
-            monthNumber++;
-            deposit = deposit + 15000;
-            System.out.println("Месяц " + monthNumber + ", сумма накоплений равна: " + deposit);
-        }
+        int[] arr1 = new int[]{1, 2, 3};
+        float[] arr2 = new float[]{1.57f, 7.654f, 9.986f};
+        char[] arr3 = {'a', 'b', 'c', 'd', 'e', 'f', 'g'};
         System.out.println();
         System.out.println();
 
@@ -34,15 +32,32 @@ public class Main {
         System.out.println("=======");
         System.out.println();
 
-        int j = 0;
-        while (j < 10) {
-            j++;
-            System.out.print(j + " ");
+        int lastIndex = arr1.length - 1;
+        for (int j = 0; j <= lastIndex; j++) {
+            System.out.print(arr1[j]);
+            if (j != lastIndex) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
-        for (int i = 10; i >= 1; i -= 1) {
-            System.out.print(i + " ");
+
+        lastIndex = arr2.length - 1;
+        for (int j = 0; j <= lastIndex; j++) {
+            System.out.print(arr2[j]);
+            if (j != lastIndex) {
+                System.out.print(", ");
+            }
         }
+        System.out.println();
+
+        lastIndex = arr3.length - 1;
+        for (int j = 0; j <= lastIndex; j++) {
+            System.out.print(arr3[j]);
+            if (j != lastIndex) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
         System.out.println();
         System.out.println();
 
@@ -54,13 +69,35 @@ public class Main {
         System.out.println("=======");
         System.out.println();
 
-        int peopleTotal = 12_000_000;
-        for (int yearNumber = 1; yearNumber <= 10; yearNumber += 1) {
-            peopleTotal = peopleTotal + peopleTotal / 1000 * (17 - 8);
-            System.out.println("Год " + yearNumber + ", численность населения составляет " + peopleTotal + ".");
+        lastIndex = arr1.length - 1;
+        for (int j = lastIndex; j >= 0; j--) {
+            System.out.print(arr1[j]);
+            if (j != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        lastIndex = arr2.length - 1;
+        for (int j = lastIndex; j >= 0; j--) {
+            System.out.print(arr2[j]);
+            if (j != 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        lastIndex = arr3.length - 1;
+        for (int j = lastIndex; j >= 0; j--) {
+            System.out.print(arr3[j]);
+            if (j != 0) {
+                System.out.print(", ");
+            }
         }
         System.out.println();
         System.out.println();
+        System.out.println();
+
 
         // Task 4
 
@@ -69,100 +106,14 @@ public class Main {
         System.out.println("=======");
         System.out.println();
 
-        double deposit2 = 15000;
-        monthNumber = 0;
-        do {
-            monthNumber++;
-            deposit2 = deposit2 * 1.07;
-            System.out.print("Месяц " + monthNumber + ", сумма накоплений равна: ");
-            System.out.printf("%.2f", deposit2);
-            System.out.println(" рублей.");
-        }
-        while (deposit2 < 12_000_000);
-        System.out.println();
-        System.out.println();
-
-        // Task 5
-
-        taskN += 1;
-        System.out.println("Task " + taskN + ".");
-        System.out.println("=======");
-        System.out.println();
-
-        deposit2 = 15000;
-        monthNumber = 0;
-        do {
-            monthNumber++;
-            deposit2 = deposit2 * 1.07;
-            if (monthNumber % 6 == 0) {
-                System.out.print("Месяц " + monthNumber + ", сумма накоплений равна: ");
-                System.out.printf("%.2f", deposit2);
-                System.out.println(" рублей.");
+        //  System.out.println(Arrays.toString(arr1));
+        lastIndex = arr1.length - 1;
+        for (int j = lastIndex; j >= 0; j--) {
+            if (arr1[j] % 2 != 0) {
+                arr1[j]++;
             }
         }
-        while (deposit2 < 12_000_000);
-        System.out.println();
-        System.out.println();
-
-        // Task 6
-
-        taskN += 1;
-        System.out.println("Task " + taskN + ".");
-        System.out.println("=======");
-        System.out.println();
-
-        deposit2 = 15000;
-        monthNumber = 1;
-        int totalMonths = 12 * 9;
-        for (; monthNumber <= totalMonths; monthNumber++) {
-            deposit2 = deposit2 * 1.07;
-            if (monthNumber % 6 == 0) {
-                System.out.print("За " + (double) monthNumber / 12 + " лет сумма накоплений составит: ");
-                System.out.printf("%.2f", deposit2);
-                System.out.println(" рублей.");
-            }
-        }
-        System.out.println();
-        System.out.println();
-
-        // Task 7
-
-        taskN += 1;
-        System.out.println("Task " + taskN + ".");
-        System.out.println("=======");
-        System.out.println();
-
-        int dateOnFriday = 3;
-        do {
-            System.out.println("Сегодня пятница, " + dateOnFriday + "-е число. Необходимо подготовить отчет.");
-            dateOnFriday += 7;
-        } while (dateOnFriday <= 31);
-        System.out.println();
-        System.out.println();
-
-        // Task 8
-
-        taskN += 1;
-        System.out.println("Task " + taskN + ".");
-        System.out.println("=======");
-        System.out.println();
-
-        // период обращения кометы
-        // int cometArrivalPeriod = 79;
-        // текущий год
-        // int currentYear = 2024;
-        // Определение года начала исследуемого интервала
-        int startYear = 2024 - 200;
-        // Определение года окончания исследуемого интервала
-        int endYear = 2024 + 100;
-        // Определение первого появления кометы в исследуемом интервале
-        int cometArrivalYear = startYear - startYear % 79 + 79;
-        do {
-//            System.out.println("startYear % cometArrivalPeriod " + startYear % cometArrivalPeriod);
-            System.out.println(cometArrivalYear);
-            cometArrivalYear += 79;
-        } while (cometArrivalYear <= endYear);
-
+        System.out.println(Arrays.toString(arr1));
         System.out.println();
         System.out.println();
 
